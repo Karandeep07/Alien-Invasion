040afa887e0e2660c8e59ae384f9e033ecc48b95
Karandeep Padam <41012770+Karandeep07@users.noreply.github.com>
2022-11-25 19:32:07 +0530

    Import Ship class

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ class Settings:
         """Initialize game settings"""
         
         # Screen Settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1000
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)     # TODO: Change to video bg
