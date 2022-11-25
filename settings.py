6f7b5efed85f7604ef1ffd2ec12f98bf1e8d0bcc
Karandeep Padam <41012770+Karandeep07@users.noreply.github.com>
2022-11-25 11:57:46 +0530

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize game settings"""
+        
+        # Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)     # TODO: Change to video bg
