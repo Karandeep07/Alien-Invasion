ab78525b467846ac4a83f61ab9d444a65c35de17
Karandeep Padam <41012770+Karandeep07@users.noreply.github.com>
2022-11-30 19:00:29 +0530

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,6 +15,12 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height)
         )
+
+        # Fullscreen
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+
         self.bg_image = pygame.transform.smoothscale(self.settings.bg_image, 
             self.screen.get_size())
         pygame.display.set_caption("Alien Invasion")
