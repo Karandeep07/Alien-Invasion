ee7334bb7e66ebc32470b29c2f7a17cf4d6adb6f
Karandeep Padam <41012770+Karandeep07@users.noreply.github.com>
2023-03-23 15:27:50 +0530

    Add Alien Fleet

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Settings:
         self.bg_image = pygame.image.load('images/background.png')
 
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 0.5  # Original = 1.5
 
         # Bullet settings
         self.bullet_speed = 1.0
