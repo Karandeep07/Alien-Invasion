a88f64d3d320485be9e505e4b3980c79b74d8b44
Karandeep Padam <41012770+Karandeep07@users.noreply.github.com>
2023-04-27 11:50:51 +0530

    Update alien.py

--- alien.py
+++ alien.py
@@ -15,7 +15,7 @@ class Alien(Sprite):
         self.rect = self.image.get_rect()
 
         # Start each new alien near the top left of the screen
-        self.rect.x = self.rect.width
+        self.rect.x = self.rect.width 
         self.rect.y = self.rect.height
 
         # Store the alien's exact horizontal position
